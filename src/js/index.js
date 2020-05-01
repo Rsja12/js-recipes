7760caf844f058e1635b6a98bc0bb9fe1ad8ac7c
Rsja12 <asocarras10@gmail.com>
2020-05-01 18:20:31 -0400

    update controlSearch function

--- src/js/index.js
+++ src/js/index.js
@@ -16,8 +16,9 @@ const controlSearch = async () => {
     if ( query ) {
         // new search obj and add to state
         state.search = new Search(query)
-        // prepare UI for reults..clear input 
+        // prepare UI for reults..clear input and clear results for next search
         searchView.clearInput()
+        searchView.clearResults()
         // Search for recipes and *await*(pause) the app and get the results before using them
         await state.search.getResults()
         // render results on UI 
