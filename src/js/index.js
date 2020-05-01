f7590eb2b640bb4ee98aa53e2eaa44a127cb41e1
Rsja12 <asocarras10@gmail.com>
2020-05-01 18:16:14 -0400

    create clearInput function

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,7 @@ const controlSearch = async () => {
         // new search obj and add to state
         state.search = new Search(query)
         // prepare UI for reults
-
+        
         // Search for recipes and *await*(pause) the app and get the results before using them
         await state.search.getResults()
         // render results on UI 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,6 +3,10 @@ import { elements } from './base'
 // return value of user input field
 export const getInput = () => elements.searchInput.value 
 
+export const clearInput = () => {
+    elements.searchInput.value = ''
+}
+
 const renderRecipe = recipe => {
 
     const markup = `
