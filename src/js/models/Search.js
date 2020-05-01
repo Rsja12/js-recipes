3948a42e214b5f0f62fc0d006fd0a86180e83489
Rsja12 <asocarras10@gmail.com>
2020-05-01 17:11:22 -0400

    create Search class for Search model

---
+++ src/js/models/Search.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export default class Search {
+    constructor(query) {
+        this.query = query 
+        this.BASE_URL = 'https://forkify-api.herokuapp.com/api/search?q='
+    }
+
+    async getResults() {
+
+        try {
+            const res = await axios.get(`${this.BASE_URL}${this.query}`)
+            this.results = res.data.recipes
+            console.log(this.results)
+        } 
+        catch(error) {
+            console.log(error)
+        }
+    
+    }
+}
+
+
