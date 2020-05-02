fb68250c44285ee22038717a8549c0dace893b50
Rsja12 <asocarras10@gmail.com>
2020-05-02 16:05:45 -0400

    create limitRecipeTitle function

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,6 +11,25 @@ export const clearResults = () => {
     elements.searchResultList.innerHTML = ''
 }
 
+// algo for limiting title length on UI
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = []
+
+    if ( title.length > limit ) {
+
+        title.split(' ').reduce( (acc, cur) => {
+            if ( acc + cur.length <= limit ) {
+                newTitle.push(cur)
+            }
+            return acc + cur.length 
+        }, 0 )
+
+        return `${newTitle.join(' ')} ...`
+    }
+
+    return title 
+}
+
 const renderRecipe = recipe => {
 
     const markup = `
