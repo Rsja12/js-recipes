a8aae8533135ba33453a2da6a4b209526ce0c244
Rsja12 <asocarras10@gmail.com>
2020-05-02 16:13:58 -0400

    create loader html

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,3 +3,13 @@ export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchResultList: document.querySelector('.results__list')
 }
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="loader">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `
+}
