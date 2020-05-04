b16a2ba9df092b10b958050f3397343519dbac2b
Rsja12 <asocarras10@gmail.com>
2020-05-04 11:35:29 -0400

    change place of api

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -4,4 +4,13 @@ export default class Recipe {
     constructor(id) {
         this.id = id
     }
+
+    async getRecipe() {
+        try {
+            const result = await axios()
+        } catch (error){
+            console.log(error)
+        }
+    }
+
 }
